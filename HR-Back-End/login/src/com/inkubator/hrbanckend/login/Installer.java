be3c420eb7619dfcd31f46b7c8d998bcc9453d3c
Deni Husni FR <Deni Husni FR@Positron>
2014-08-04 10:44:10 +0700

    Initialisasi

--- HR-Back-End/login/src/com/inkubator/hrbanckend/login/Installer.java
+++ HR-Back-End/login/src/com/inkubator/hrbanckend/login/Installer.java
@@ -5,13 +5,27 @@
  */
 package com.inkubator.hrbanckend.login;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import org.openide.DialogDescriptor;
+import org.openide.DialogDisplayer;
 import org.openide.modules.ModuleInstall;
 
-public class Installer extends ModuleInstall {
+public class Installer extends ModuleInstall implements ActionListener{
+
+    private LoginForm loginForm=new LoginForm();
+    private DialogDescriptor dd=null;
 
     @Override
     public void restored() {
-        // TODO
+        dd=new DialogDescriptor(loginForm, "Login", true, this);
+//        dd.set
+        DialogDisplayer.getDefault().notifyLater(dd);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 }
